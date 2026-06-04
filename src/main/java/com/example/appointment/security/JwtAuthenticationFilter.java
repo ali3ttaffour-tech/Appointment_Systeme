04f1ebe06d46ae03eea7f6c51e77e91fa1df60cc
DEL <alittafour32@gmail.com>
2026-06-04 12:12:40 +0300

    add permaisinn

--- src/main/java/com/example/appointment/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/appointment/security/JwtAuthenticationFilter.java
@@ -23,10 +23,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
+
+
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
+
+        System.out.println("JWT FILTER WORKING");
 
         String path = request.getRequestURI();
 
